93cbfd7316b4e5c3523deb30de65aa832fc4fb8d
David Browne <davidbrowne@outlook.com>
2017-04-28 00:22:50 -0700

    Fixed up TD_lambda to use updates to RL_lib

--- FA/TD_lambda_VFAHeader.py
+++ FA/TD_lambda_VFAHeader.py
@@ -13,6 +13,7 @@ from lib.RL_Library import ValueEstimator, TD_lambda, LSPI
 matplotlib.style.use('ggplot')
 
 test = "MountainCar-v0"
+state_labels = ['x', 'x_dot']
 #test = "CartPole-v1"
 
 env = gym.envs.make(test)
@@ -26,7 +27,7 @@ if loadmodel:
 
 else:
     weights = None
-    estimator = ValueEstimator(env, weights)
+    estimator = ValueEstimator(env, weights, state_labels)
     estimator = LSPI(env, estimator)
     # stats, estimator = TD_lambda(env, estimator, 10000, EpsilonFunction.DecayEpsilonFunction, display=False, alpha=1e-3)
 
